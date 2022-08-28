c7ae08785fbb10434c5226e53ceb6e0ef72a11bf
Nedzla Uzunalic <nedzla.uzunalic@edu.fit.ba>
2022-08-28 15:25:50 +0200

    fix

--- eBotanika.API/Controllers/UposlenikUlogeController.cs
+++ eBotanika.API/Controllers/UposlenikUlogeController.cs
@@ -1,13 +1,10 @@
 ﻿using eBotanika.Services.UposlenikUloge;
-
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eBotanika.API.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    [AllowAnonymous]
     public class UposlenikUlogeController : ControllerBase
     {
         private readonly IUposlenikUlogeService _service;
